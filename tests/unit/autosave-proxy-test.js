e7e36533f5739f3699109b9bc8f5b77a828c904f
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2016-04-30 23:36:26 +0200

    setting value to same value should not trigger save

--- tests/unit/autosave-proxy-test.js
+++ tests/unit/autosave-proxy-test.js
@@ -67,3 +67,12 @@ test('changing the content flushes a pending save', function(assert) {
   autosaveObject.set('content', {});
   assert.ok(model.save.called, 'save was called before the content changed');
 });
+
+test('setting a property to the same value', function(assert) {
+  model.set('will-set-same-value', 1);
+  autosaveObject.set('will-set-same-value', 1);
+  assert.ok(!model.save.called, 'save was not called immediately');
+
+  clock.tick(1000);
+  assert.ok(!model.save.called, 'save was called on same value');
+});
