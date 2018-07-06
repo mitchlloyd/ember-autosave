e43e54bef6712eb0a7f0af719670e0dae33eba47
Mitch Lloyd <mitch.lloyd@gmail.com>
2018-07-05 21:24:05 -0700

    Add failing test for #31

--- tests/integration/ember-observation-test.js
+++ tests/integration/ember-observation-test.js
@@ -1,5 +1,6 @@
 import { AutosaveProxy } from 'ember-autosave';
 import { moduleForComponent, test } from 'ember-qunit';
+import Ember from 'ember';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('ember-observation', {
@@ -20,4 +21,45 @@ test('AutosaveProxy notifies Ember of property changes', function(assert) {
   this.$('input').val('new').change();
 
   assert.equal(this.$('#output').text().trim(), 'new', 'Ember was notified of value change');
+});
+
+test("Framework Test - Ember set doesn't notify listeners when new value === the previous one", function(assert) {
+  let renders = 0;
+  const countRenders = () => ++renders;
+  this.register('helper:count-renders', Ember.Helper.helper(countRenders));
+
+  this.set('model', { prop: 1 });
+
+  this.render(hbs`
+    {{count-renders}}
+    <div id="output">{{model.prop}}</div>
+  `);
+
+  assert.equal(this.$('#output').text().trim(), '1', 'precondition - Ember gets the original value');
+  assert.equal(renders, 1, 'rendered once');
+
+  Ember.set(this.model, 'prop', 1);
+  assert.equal(this.$('#output').text().trim(), '1', 'New value is the same');
+  assert.equal(renders, 1, 'did not rerender');
+});
+
+test("AutosaveProxy doesn't notify listeners when value === the previous value", function(assert) {
+  let renders = 0;
+  const countRenders = () => ++renders;
+  this.register('helper:count-renders', Ember.Helper.helper(countRenders));
+
+  let model = { prop: 1 };
+  this.set('proxy', AutosaveProxy.create({ content: model }));
+
+  this.render(hbs`
+    {{count-renders}}
+    <div id="output">{{proxy.prop}}</div>
+  `);
+
+  assert.equal(this.$('#output').text().trim(), '1', 'precondition - Ember gets the original value');
+  assert.equal(renders, 1, 'rendered once');
+
+  Ember.set(this.proxy, 'prop', 1);
+  assert.equal(this.$('#output').text().trim(), '1', 'New value is the same');
+  assert.equal(renders, 1, 'did not rerender');
 });
