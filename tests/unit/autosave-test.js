4a516f395741c9a6c7aeb445a7a39edd95fbbd7c
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-02 02:13:21 -0400

    Ran codemods to remove jQuery

--- tests/unit/autosave-test.js
+++ tests/unit/autosave-test.js
@@ -6,90 +6,90 @@ import { module, test } from 'qunit';
 let model;
 let clock;
 
-module('Using autosave computed property', {
-  beforeEach: function() {
+module('Using autosave computed property', function(hooks) {
+  hooks.beforeEach(function() {
     model = EmberObject.create({ save: sinon.spy() });
     clock = sinon.useFakeTimers();
-  },
+  });
 
-  afterEach: function() {
+  hooks.afterEach(function() {
     clock.restore();
-  }
-});
-
-test('setting a property eventually saves the model with the property', function(assert) {
-  let Component = EmberObject.extend({
-    autosaveObject: autosave('model')
   });
 
-  let component = Component.create({ model: model });
+  test('setting a property eventually saves the model with the property', function(assert) {
+    let Component = EmberObject.extend({
+      autosaveObject: autosave('model')
+    });
 
-  set(component, 'autosaveObject.name', 'Millie');
+    let component = Component.create({ model: model });
 
-  assert.ok(!model.save.called, 'save was not called immediately');
-  clock.tick(1000);
-  assert.ok(model.save.called, 'save was called after ellapsed time');
-});
+    set(component, 'autosaveObject.name', 'Millie');
 
-test('many sets are debounced', function(assert) {
-  let Component = EmberObject.extend({
-    autosaveObject: autosave('model')
+    assert.ok(!model.save.called, 'save was not called immediately');
+    clock.tick(1000);
+    assert.ok(model.save.called, 'save was called after ellapsed time');
   });
 
-  let component = Component.create({ model: model });
+  test('many sets are debounced', function(assert) {
+    let Component = EmberObject.extend({
+      autosaveObject: autosave('model')
+    });
 
-  set(component, 'autosaveObject.name', '1');
-  set(component, 'autosaveObject.name', '2');
-  set(component, 'autosaveObject.name', 'Millie');
+    let component = Component.create({ model: model });
 
-  assert.ok(!model.save.called, 'save was not called immediately');
-  clock.tick(1000);
-  assert.equal(model.save.callCount, 1, 'save was called once after ellapsed time');
-});
+    set(component, 'autosaveObject.name', '1');
+    set(component, 'autosaveObject.name', '2');
+    set(component, 'autosaveObject.name', 'Millie');
 
-test('calling flushPendingSave immediately saves the target', function(assert) {
-  let Component = EmberObject.extend({
-    autosaveObject: autosave('model')
+    assert.ok(!model.save.called, 'save was not called immediately');
+    clock.tick(1000);
+    assert.equal(model.save.callCount, 1, 'save was called once after ellapsed time');
   });
 
-  let component = Component.create({ model: model });
-  set(component, 'autosaveObject.name', 'Millie');
+  test('calling flushPendingSave immediately saves the target', function(assert) {
+    let Component = EmberObject.extend({
+      autosaveObject: autosave('model')
+    });
 
-  assert.ok(!model.save.called, 'save was not called immediately');
-  flushPendingSave(component.get('autosaveObject'));
-  assert.ok(model.save.called, 'save was called after setting new model');
-});
+    let component = Component.create({ model: model });
+    set(component, 'autosaveObject.name', 'Millie');
 
-test("using the computed property context and string for save", function(assert) {
-  let Component = EmberObject.extend({
-    autosaveModel: autosave('model', { save: 'specialSave' }),
-
-    specialSave: function(model) {
-      model.save();
-    }
+    assert.ok(!model.save.called, 'save was not called immediately');
+    flushPendingSave(component.get('autosaveObject'));
+    assert.ok(model.save.called, 'save was called after setting new model');
   });
 
-  let component = Component.create({ model: model });
-  component.set('autosaveModel.name', 'Millie');
-  clock.tick(1000);
-  assert.ok(model.save.called, 'save was called after ellapsed time');
-});
+  test("using the computed property context and string for save", function(assert) {
+    let Component = EmberObject.extend({
+      autosaveModel: autosave('model', { save: 'specialSave' }),
 
-test('specifying a save function without content key', function(assert) {
-  let Component = EmberObject.extend({
-    someProp: 'some-prop',
-
-    person: autosave({
-      save: function(attrs) {
-        model.save(attrs, this.someProp);
+      specialSave: function(model) {
+        model.save();
       }
-    })
+    });
+
+    let component = Component.create({ model: model });
+    component.set('autosaveModel.name', 'Millie');
+    clock.tick(1000);
+    assert.ok(model.save.called, 'save was called after ellapsed time');
   });
 
-  let component = Component.create();
-  set(component, 'person.name', 'Millie');
+  test('specifying a save function without content key', function(assert) {
+    let Component = EmberObject.extend({
+      someProp: 'some-prop',
 
-  clock.tick(1000);
-  assert.ok(model.save.called, 'save was called after ellapsed time');
-  assert.ok(model.save.calledWith({ name: 'Millie' }, 'some-prop'), "called with correct arguments");
+      person: autosave({
+        save: function(attrs) {
+          model.save(attrs, this.someProp);
+        }
+      })
+    });
+
+    let component = Component.create();
+    set(component, 'person.name', 'Millie');
+
+    clock.tick(1000);
+    assert.ok(model.save.called, 'save was called after ellapsed time');
+    assert.ok(model.save.calledWith({ name: 'Millie' }, 'some-prop'), "called with correct arguments");
+  });
 });
--- tests/unit/config-test.js
+++ tests/unit/config-test.js
@@ -5,73 +5,73 @@ import { AutosaveProxy } from 'ember-autosave';
 
 let clock;
 
-module('Configuration', {
-  beforeEach: function() {
+module('Configuration', function(hooks) {
+  hooks.beforeEach(function() {
     clock = sinon.useFakeTimers();
-  },
-
-  afterEach: function() {
-    clock.restore();
-    AutosaveProxy.config({});
-  }
-});
-
-test('globally overriding saveDelay', function(assert) {
-  AutosaveProxy.config({ saveDelay: 500 });
-  let model = EmberObject.create({ save: sinon.spy() });
-  let autosaveObject = AutosaveProxy.create(model);
-
-  autosaveObject.set('name', 'Millie');
-  clock.tick(500);
-  assert.ok(model.save.called, 'save was called after ellapsed time');
-});
-
-test('locally overriding saveDelay', function(assert) {
-  let model = EmberObject.create({ save: sinon.spy() });
-  let autosaveObject = AutosaveProxy.create(model, { saveDelay: 250 });
-
-  autosaveObject.set('name', 'Millie');
-  clock.tick(250);
-  assert.ok(model.save.called, 'save was called after ellapsed time');
-});
-
-test('globally overriding save function', function(assert) {
-  AutosaveProxy.config({
-    save: function(model) {
-      model.configuredSave();
-    }
   });
 
-  let model = EmberObject.create({ configuredSave: sinon.spy() });
-  let autoSaveObject = AutosaveProxy.create(model);
+  hooks.afterEach(function() {
+    clock.restore();
+    AutosaveProxy.config({});
+  });
 
-  autoSaveObject.set('name', 'Millie');
-  clock.tick(1000);
-  assert.ok(model.configuredSave.called, 'save was called after ellapsed time');
-});
+  test('globally overriding saveDelay', function(assert) {
+    AutosaveProxy.config({ saveDelay: 500 });
+    let model = EmberObject.create({ save: sinon.spy() });
+    let autosaveObject = AutosaveProxy.create(model);
 
-test('Configuring only fields - only saves when specified field is triggred', function(assert) {
-  let model = EmberObject.create({ save: sinon.spy() });
-  let autoSaveObject = AutosaveProxy.create(model, { only: ['name'] });
+    autosaveObject.set('name', 'Millie');
+    clock.tick(500);
+    assert.ok(model.save.called, 'save was called after ellapsed time');
+  });
 
-  autoSaveObject.set('age', 97);
-  clock.tick(1000);
-  assert.ok(!model.save.called, 'save was not called');
+  test('locally overriding saveDelay', function(assert) {
+    let model = EmberObject.create({ save: sinon.spy() });
+    let autosaveObject = AutosaveProxy.create(model, { saveDelay: 250 });
 
-  autoSaveObject.set('name', 'Millie');
-  clock.tick(1000);
-  assert.ok(model.save.called, 'save was not called after ellapsed time');
-});
+    autosaveObject.set('name', 'Millie');
+    clock.tick(250);
+    assert.ok(model.save.called, 'save was called after ellapsed time');
+  });
 
-test('configuring `except` fields - only saves when specified field is triggred', function(assert) {
-  let model = EmberObject.create({ save: sinon.spy() });
-  let autoSaveObject = AutosaveProxy.create(model, { except: ['age'] });
+  test('globally overriding save function', function(assert) {
+    AutosaveProxy.config({
+      save: function(model) {
+        model.configuredSave();
+      }
+    });
 
-  autoSaveObject.set('age', 97);
-  clock.tick(1000);
-  assert.ok(!model.save.called, 'save was not called on `except` property');
+    let model = EmberObject.create({ configuredSave: sinon.spy() });
+    let autoSaveObject = AutosaveProxy.create(model);
 
-  autoSaveObject.set('name', 'Millie');
-  clock.tick(1000);
-  assert.ok(model.save.called, 'save was called');
+    autoSaveObject.set('name', 'Millie');
+    clock.tick(1000);
+    assert.ok(model.configuredSave.called, 'save was called after ellapsed time');
+  });
+
+  test('Configuring only fields - only saves when specified field is triggred', function(assert) {
+    let model = EmberObject.create({ save: sinon.spy() });
+    let autoSaveObject = AutosaveProxy.create(model, { only: ['name'] });
+
+    autoSaveObject.set('age', 97);
+    clock.tick(1000);
+    assert.ok(!model.save.called, 'save was not called');
+
+    autoSaveObject.set('name', 'Millie');
+    clock.tick(1000);
+    assert.ok(model.save.called, 'save was not called after ellapsed time');
+  });
+
+  test('configuring `except` fields - only saves when specified field is triggred', function(assert) {
+    let model = EmberObject.create({ save: sinon.spy() });
+    let autoSaveObject = AutosaveProxy.create(model, { except: ['age'] });
+
+    autoSaveObject.set('age', 97);
+    clock.tick(1000);
+    assert.ok(!model.save.called, 'save was not called on `except` property');
+
+    autoSaveObject.set('name', 'Millie');
+    clock.tick(1000);
+    assert.ok(model.save.called, 'save was called');
+  });
 });
